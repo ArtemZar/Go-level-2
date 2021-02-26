d3458b8636363787a152653b1653c2d3eb67d9f1
ArtemZar <ZarubinAV178@gmail.com>
2021-02-26 15:26:17 +0300

    Документация к проекту

---
+++ lesson-2/fibonachi/example_test.go
@@ -0,0 +1,11 @@
+package fibonachi
+
+import (
+	"fmt"
+	"github.com/ArtemZar/Go-level-2/lesson-2/fibonachi"
+)
+
+func ExampleFindFibonachiElement() {
+	fmt.Println(fibonachi.FindFibonachiElement(9))
+	//Output 34
+}
